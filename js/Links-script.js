2959bc15e0bcc8105ada814c9a1809a4063fb545
H.R <hr9084393@gmail.com>
2026-04-07 10:09:55 +0200

    Delete js directory

--- js/Links-script.js
+++
@@ -1,7 +0,0 @@
-/**
- * وظيفة تبديل وضع الألوان (Dark Mode / Light Mode)
- * تقوم بإضافة أو إزالة كلاس "light" من عنصر الـ body
- */
- function toggleMode() {
-    document.body.classList.toggle("light");
-  }
